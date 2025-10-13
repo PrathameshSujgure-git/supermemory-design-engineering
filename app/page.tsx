2300d103fdbc99c23c1e1e5000ef922e4de1dddd
Apple Mac <applemac@PdS-10k-Mac.local>
2025-10-14 02:56:45 +0530

    w

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className='flex min-h-screen items-center justify-center'>
       <div className='relative flex justify-center items-center'>
-        <div className='relative flex place-items-center justify-center z-10 blur-[6px]'>
+        <div className='relative flex items-center justify-center z-10 blur-[6px]'>
           <NovaOrb size={300} />
         </div>
         <div className='absolute z-0 opacity-40 top-[10px] blur-[10px]'>
